6a57a32afa5457c94029f32df227bdcfdd9cd84b
tserenhuu <tserenhuumonhbat@gmail.com>
2025-04-28 20:55:23 +0800

    task vzvvleltvvdiig haruuldag bolson.

--- frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
+++ frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
@@ -11,6 +11,11 @@ import { addThousandsSeparator } from '../../utils/helper';
 import InfoCard from '../../components/Cards/InfoCard';
 import { LuArrowRight } from 'react-icons/lu';
 import TaskListTable from '../../components/TaskListTable';
+import CustomPieChart from '../../components/Charts/CustomPieChart';
+import CustomBarChart from '../../components/Charts/CustomBarChart';
+
+const COLORS = ["#8D51FF", "#00B8DB", "#7BCE00"];
+
 
 const Dashboard = () => {
   useUserAuth();
@@ -23,6 +28,28 @@ const Dashboard = () => {
   const [pieChartData, setPieChartData] = useState([]);
   const [barChartData, setBarChartData] = useState([]);
 
+  // Prepare Chart Data
+  const prepareChartData = (data) => {
+    const taskDistribution = data?.taskDistribution || null;
+    const taskPriorityLevels = data?.taskPriorityLevels || null;
+
+    const taskDistributionData = [
+      {status: "Pending", count: taskDistribution?.Pending || 0},
+      {status: "In Progress", count: taskDistribution?.InProgress || 0},
+      {status: "Completed", count: taskDistribution?.Completed || 0}, 
+    ];
+
+    setPieChartData(taskDistributionData);
+
+    const PriorityLevelData = [
+      {priority: "Low", count: taskPriorityLevels?.Low || 0 },
+      {priority: "Medium", count: taskPriorityLevels?.Medium || 0 },
+      {priority: "High", count: taskPriorityLevels?.High || 0 },
+    ];
+
+    setBarChartData(PriorityLevelData);
+  }
+
   const getDashboardData = async () => {
     try{
       const response = await axiosInstance.get(
@@ -30,6 +57,7 @@ const Dashboard = () => {
       );
       if(response.data){
         setDashboardData(response.data);
+        prepareChartData(response.data?.charts || null);
       }
     }catch(error){
       console.log("Error fetching users:", error);
@@ -50,14 +78,18 @@ const Dashboard = () => {
     <DashboardLayout activeMenu="Dashboard">
       {JSON.stringify(dashboardData)}
       <div className='card my-5'>
-        <div className='col-span-3'>
-          <h2 className='text-xl md:text-2x1'>Good Morning! {user?.name}</h2>
-          <p className='text-xs md:text-[13px] text-gray-400 mt-1.5'>
-            {moment().format("dddd Do MMM YYYY")}
-          </p>
+        <div>
+          <div className='col-span-3'>
+            <h2 className='text-xl md:text-2x1'>Good Morning! {user?.name}</h2>
+            <p className='text-xs md:text-[13px] text-gray-400 mt-1.5'>
+              {moment().format("dddd Do MMM YYYY")}
+            </p>
+          </div>
         </div>
 
         <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-3 md:gap-6  mt-5'>
+
+
           <InfoCard
             label="Total Tasks"
             value={addThousandsSeparator(
@@ -93,6 +125,32 @@ const Dashboard = () => {
       </div>
 
       <div className='grid grid-cols-1 md:grid-cols-2 gap-6 my-4 md:my-6'>
+        
+        <div>
+          <div className='card'>
+            <div className='flex items-center justify-between'>
+              <h5 className='font-medium'>Task Distribution</h5>
+            </div>
+
+            <CustomPieChart
+              data={pieChartData}
+              colors={COLORS}
+            />
+          </div>
+        </div>
+
+        <div>
+          <div className='card'>
+            <div className='flex items-center justify-between'>
+              <h5 className='font-medium'>Task Priority Levels</h5>
+            </div>
+
+            <CustomBarChart
+              data={barChartData}
+            />
+          </div>
+        </div>
+
         <div className='md:col-span-2'>
           <div className='card'>
             <div className='flex items-center justify-between'>
